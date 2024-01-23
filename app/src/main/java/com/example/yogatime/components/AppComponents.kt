ee158cbf9dbe67c0214e79372e4fad120d181744
roy wasker <royx520@gmail.com>
2024-01-23 21:33:56 +0200

    add toolbar

--- app/src/main/java/com/example/yogatime/components/AppComponents.kt
+++ app/src/main/java/com/example/yogatime/components/AppComponents.kt
@@ -54,11 +54,13 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.ImeAction
@@ -70,6 +72,57 @@ import com.example.yogatime.data.rules.NavigationItem
 import com.example.yogatime.ui.theme.AccentColor
 import com.example.yogatime.ui.theme.Secondary
 import com.example.yogatime.ui.theme.WhiteColor
+
+
+
+import android.app.DatePickerDialog
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.TextField
+
+import androidx.compose.runtime.*
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.toSize
+import androidx.lifecycle.liveData
+import com.example.yogatime.data.gallery.GalleryScreenViewModel
+import java.util.*
+
+import coil.compose.AsyncImage
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.storage
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+
 
 
 
@@ -144,6 +197,46 @@ fun MyTextField(labelValue: String, painterResource: Painter,
 }
 
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyPhoneField(labelValue: String, painterResource: Painter,
+                onTextSelected: (String) -> Unit,
+                errorStatus:Boolean = false) {
+    val textValue = remember {
+        mutableStateOf("")
+    }
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(componentShapes.small),
+        label = { Text(text = labelValue)},
+        value = textValue.value,
+        textStyle = TextStyle(fontSize = 18.sp),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Primary,
+            focusedLabelColor = Primary,
+            cursorColor = Primary,
+
+            ),
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Phone,
+            imeAction = ImeAction.Next
+        ),
+        singleLine = true,
+        maxLines = 1,
+        onValueChange = {
+            textValue.value = it
+            onTextSelected(it)
+        },
+        leadingIcon = {
+            Icon(painter = painterResource, contentDescription ="" )
+        },
+        isError = !errorStatus
+    )
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PasswordTextField(labelValue: String, painterResource: Painter,
@@ -172,7 +265,7 @@ fun PasswordTextField(labelValue: String, painterResource: Painter,
             focusedLabelColor = Primary,
             cursorColor = Primary,
             ),
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Done),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Next),
         singleLine = true,
         keyboardActions = KeyboardActions{
             localFocusManager.clearFocus()
@@ -208,6 +301,167 @@ fun PasswordTextField(labelValue: String, painterResource: Painter,
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+@Composable
+fun MyEmailField(labelValue: String,
+                 painterResource: Painter,
+                 onTextSelected: (String) -> Unit,
+                 errorStatus:Boolean = false) {
+    val textValue = remember {
+        mutableStateOf("")
+    }
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(componentShapes.small),
+        label = { Text(text = labelValue)},
+        value = textValue.value,
+        textStyle = TextStyle(fontSize = 18.sp),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Primary,
+            focusedLabelColor = Primary,
+            cursorColor = Primary,
+
+            ), keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Email,
+            imeAction = ImeAction.Next
+        ),
+        singleLine = true,
+        maxLines = 1,
+        onValueChange = {
+            textValue.value = it
+            onTextSelected(it)
+        },
+        leadingIcon = {
+            Icon(painter = painterResource, contentDescription ="" )
+        },
+        isError = !errorStatus
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DateFromTodayPicker(
+    labelValue: String,
+    painterResource: Painter,
+    onDateSelected: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val context = LocalContext.current
+    var selectedDate by remember { mutableStateOf("") }
+    val calendar = Calendar.getInstance()
+    val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+        // Note: month is 0 based
+        selectedDate = "$dayOfMonth/${month + 1}/$year"
+        onDateSelected(selectedDate) // Pass the selected date back to the caller
+        // Optionally display a message or handle the date selection as required
+    }
+
+    OutlinedTextField(
+        value = selectedDate,
+        onValueChange = { /* Read-only field */ },
+        readOnly = true,
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(componentShapes.small),
+        textStyle = TextStyle(fontSize = 18.sp),
+        label = { Text(text = labelValue) },
+
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Primary,
+            focusedLabelColor = Primary,
+            cursorColor = Primary,
+        ),
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Next
+        ), singleLine = true,
+        maxLines = 1,
+        trailingIcon = {
+            Icon(
+                imageVector = Icons.Default.DateRange,
+                contentDescription = "Select Date",
+                modifier = Modifier.clickable {
+                    DatePickerDialog(
+                        context,
+                        dateSetListener,
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)
+                    ).apply {
+                        datePicker.minDate = System.currentTimeMillis() - 1000 // Restrict to today or future dates
+                        show()
+                    }
+                }
+            )
+        })
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BirthdayDatePicker(
+    labelValue: String,
+    onDateSelected: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val context = LocalContext.current
+    var selectedDate by remember { mutableStateOf("") }
+    val calendar = Calendar.getInstance()
+    // Adjust the calendar to start 100 years back for a birthday picker
+    val YearsBack = calendar.apply {
+        add(Calendar.YEAR, -100)
+    }.timeInMillis
+
+    val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+        // Note: month is 0 based
+        selectedDate = "$dayOfMonth/${month + 1}/$year"
+        onDateSelected(selectedDate) // Pass the selected date back to the caller
+    }
+
+    OutlinedTextField(
+        value = selectedDate,
+        onValueChange = { /* Read-only field */ },
+        readOnly = true,
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(componentShapes.small),
+        textStyle = TextStyle(fontSize = 18.sp),
+        label = { Text(text = labelValue) },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.DateRange,
+                contentDescription = "Select Date",
+                modifier = Modifier.clickable {
+                    DatePickerDialog(
+                        context,
+                        dateSetListener,
+                        calendar.get(Calendar.YEAR) - 70, // Start 70 years back
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)
+                    ).apply {
+                        datePicker.maxDate = System.currentTimeMillis() // Ensure future dates cannot be picked
+                        datePicker.minDate = YearsBack // Optional: Set a minimum date if needed
+                                show()
+                    }
+                }
+            )
+        },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Primary,
+            focusedLabelColor = Primary,
+            cursorColor = Primary,
+        ),
+        singleLine = true,
+        maxLines = 1,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Next
+        )
+    )
+}
+
+
+
+
 @Composable
 fun ButtonComponent(value:String, onButtonClicked : () -> Unit, isEnabled : Boolean = false){
     Button(onClick = {
@@ -235,6 +489,40 @@ fun ButtonComponent(value:String, onButtonClicked : () -> Unit, isEnabled : Bool
             }
     }
 }
+
+
+@Composable
+fun DateFromTodayCompose(
+    modifier: androidx.compose.ui.Modifier = androidx.compose.ui.Modifier
+) {
+    val context = LocalContext.current
+    var selectedDate by remember { mutableStateOf("") }
+    val calendar = Calendar.getInstance()
+
+    // Prepare the listener for date set
+    val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+        // Note: month is 0 based
+        selectedDate = "$dayOfMonth/${month + 1}/$year"
+        // Use the selected date as needed
+        Toast.makeText(context, "Selected date: $selectedDate", Toast.LENGTH_LONG).show()
+    }
+
+    Button(onClick = {
+        DatePickerDialog(
+            context,
+            dateSetListener,
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        ).apply {
+            datePicker.minDate = System.currentTimeMillis() - 1000 // Restrict to today or future
+            show()
+        }
+    }, modifier = modifier) {
+        Text(if (selectedDate.isEmpty()) "Select Date" else selectedDate)
+    }
+}
+
 
 @Composable
 fun ClickableTextComponent(tryingToLogin: Boolean = true, onTextSelected: (String) -> Unit) {
@@ -324,6 +612,112 @@ fun NavigationDrawerHeader(value: String?) {
         NavigationDrawerText(
             title = value?:stringResource(R.string.navigation_header), 28.sp , AccentColor
         )
+@Composable
+fun PickImageFromGallery(viewModel: GalleryScreenViewModel) {
+
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    val context = LocalContext.current
+    val bitmap = remember { mutableStateOf<Bitmap?>(null) }
+
+    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+        imageUri = uri
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        imageUri?.let {
+            if (Build.VERSION.SDK_INT < 28) {
+                bitmap.value = MediaStore.Images.Media.getBitmap(context.contentResolver, it)
+            } else {
+                val source = ImageDecoder.createSource(context.contentResolver, it)
+                bitmap.value = ImageDecoder.decodeBitmap(source)
+            }
+
+            bitmap.value?.let { btm ->
+                Image(
+                    bitmap = btm.asImageBitmap(),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(400.dp)
+                        .padding(20.dp)
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Button(onClick = { launcher.launch("image/*") }) {
+            Text(text = "Pick Image")
+        }
+
+        // Conditionally display the Upload button only if an image has been picked
+        if (imageUri != null) {
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Button(onClick = {
+                imageUri?.let { uri ->
+                    val inputStream = context.contentResolver.openInputStream(uri)
+                    inputStream?.let {
+                        // Assuming you have a fileName strategy, for example using a timestamp
+                        val fileName = "upload_${System.currentTimeMillis()}.jpg"
+                        viewModel.uploadImageToFirebaseStorage(it, fileName)
+                    }
+                }
+            }) {
+                Text(text = "Upload")
+            }
+        }
+    }
+
+}
+
+@Composable
+fun SinglePhotoPicker(){
+    var uri by remember{
+        mutableStateOf<Uri?>(null)
+    }
+    val singlePhotoPicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = {
+            uri = it
+        }
+    )
+
+    val context = LocalContext.current
+
+
+    Column{
+
+
+        AsyncImage(model = uri, contentDescription = null, modifier = Modifier.size(248.dp))
+
+        Button(onClick = {
+            singlePhotoPicker.launch(
+                PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+            )
+
+        }){
+            Text("Pick Image")
+        }
+
+        if(uri != null) {
+
+            Button(onClick = {
+                uri?.let {
+
+                    GalleryScreenViewModel.uploadToStorage(uri = it, context = context)
+                }
+
+            }) {
+                Text("Upload")
+            }
+        }
+
+
+
 
     }
 }
@@ -373,3 +767,43 @@ fun NavigationDrawerText(title: String, textUnit: TextUnit,color: Color) {
         )
     )
 }
+fun dropDownMenu() {
+
+    var expanded by remember { mutableStateOf(false) }
+    val suggestions = listOf("Kotlin", "Java", "Dart", "Python")
+    var selectedText by remember { mutableStateOf("") }
+
+    var textfieldSize by remember { mutableStateOf(Size.Zero) }
+
+    val icon = if (expanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
+
+
+    Column(Modifier.padding(20.dp)) {
+        OutlinedTextField(
+            value = selectedText,
+            onValueChange = { selectedText = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .onGloballyPositioned { coordinates ->
+                    //This value is used to assign to the DropDown the same width
+                    textfieldSize = coordinates.size.toSize()
+                },
+            label = { Text("Label") },
+            trailingIcon = {
+                Icon(icon, "contentDescription",
+                    Modifier.clickable { expanded = !expanded })
+            }
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current) { textfieldSize.width.toDp() })
+        ) {
+        }
+    }
+}
+
